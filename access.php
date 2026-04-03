886c413f1c5d50ec5951c50c4ea9da3b336710bf
Harleen22190 <221909hkaur@gmail.com>
2026-04-03 12:17:57 +0530

    Add files via upload

---
+++ access.php
@@ -0,0 +1,24 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+
+    'local/trainingrequest:view' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+
+    'local/trainingrequest:manage' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+
+];
