0eafcabba3b77841e06472746cffb67288de7495
Harleen22190 <221909hkaur@gmail.com>
2026-04-05 11:47:24 +0530

    Update local_trainingrequest.php

--- local_trainingrequest.php
+++ local_trainingrequest.php
@@ -1,4 +1,34 @@
 <?php
+defined('MOODLE_INTERNAL') || die();
+
+
 $string['pluginname'] = 'Training Request System';
+
 $string['apply'] = 'Apply for Training';
-$string['dashboard'] = 'My Requests';
+$string['dashboard'] = 'My Dashboard';
+$string['myrequests'] = 'My Training Requests';
+
+
+$string['facultypanel'] = 'Faculty Panel';
+$string['approve'] = 'Approve';
+$string['reject'] = 'Reject';
+
+
+$string['adminpanel'] = 'Admin Panel';
+$string['assignfaculty'] = 'Assign Faculty';
+$string['totalrequests'] = 'Total Requests';
+$string['approved'] = 'Approved';
+$string['pending'] = 'Pending';
+
+$string['certificate'] = 'Certificate';
+$string['downloadcertificate'] = 'Download Certificate';
+$string['certificateid'] = 'Certificate ID';
+
+$string['verify'] = 'Verify Certificate';
+$string['validcertificate'] = 'Certificate Verified';
+$string['invalidcertificate'] = 'Invalid Certificate';
+
+
+$string['submitted'] = 'Request submitted successfully';
+$string['approvedmsg'] = 'Your request has been approved';
+$string['rejectedmsg'] = 'Your request has been rejected';
