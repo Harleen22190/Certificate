48f2ef982adbf8d8afbd1444e48fe93218d9b7f1
Harleen22190 <221909hkaur@gmail.com>
2026-04-05 11:45:28 +0530

    Update access.php

--- access.php
+++ access.php
@@ -2,23 +2,27 @@
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = [
-
-    'local/trainingrequest:view' => [
-        'captype' => 'read',
+    'local/trainingrequest:apply' => [
+        'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => [
             'student' => CAP_ALLOW,
-            'teacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW,
         ],
     ],
 
+    'local/trainingrequest:approve' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+        ],
+    ],
     'local/trainingrequest:manage' => [
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => [
             'manager' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
         ],
     ],
-
 ];
