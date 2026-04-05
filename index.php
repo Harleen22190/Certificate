b9d99bec33fc1ac2b90a06a82431a4b016a4d283
Harleen22190 <221909hkaur@gmail.com>
2026-04-05 11:42:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,30 @@
 require('../../config.php');
 require_login();
 
+$context = context_system::instance();
+global $PAGE, $OUTPUT;
+
 $PAGE->set_url('/local/trainingrequest/index.php');
-$PAGE->set_pagelayout('standard');
-$PAGE->set_title('Training Request');
-$PAGE->set_heading('Training Request');
+$PAGE->set_title('Training Request System');
+$PAGE->set_heading('Training Request System');
 
 echo $OUTPUT->header();
 
+echo "<div class='container mt-4 text-center'>";
+
 echo "<h2>Training Request System</h2>";
 
-echo "<a href='apply.php'>Apply</a><br>";
-echo "<a href='dashboard.php'>My Dashboard</a><br>";
-echo "<a href='admin.php'>Admin Panel</a><br>";
-echo "<a href='faculty.php'>Faculty Panel</a><br>";
+echo "<div class='mt-4'>";
+
+echo "<a href='apply.php' class='btn btn-success me-2'>Apply</a>";
+echo "<a href='dashboard.php' class='btn btn-primary me-2'>My Dashboard</a>";
+
+if (has_capability('local/trainingrequest:manage', context_system::instance())) {
+    echo "<a href='admin.php' class='btn btn-dark me-2'>Admin Panel</a>";
+    echo "<a href='faculty.php' class='btn btn-warning'>Faculty Panel</a>";
+}
+
+echo "</div>";
+echo "</div>";
 
 echo $OUTPUT->footer();
