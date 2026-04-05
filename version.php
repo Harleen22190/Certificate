f17a88b11611aeaed4376aa9b80c2afcd572aac2
Harleen22190 <221909hkaur@gmail.com>
2026-04-05 11:44:50 +0530

    Update version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_trainingrequest';
-$plugin->version   = 2026040300;
+$plugin->version   = 2026040402; 
 $plugin->requires  = 2022041900;
-$plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '1.0';
+$plugin->maturity  = MATURITY_STABLE; 
+$plugin->release   = '1.0 (Final)';
